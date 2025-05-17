f25bd26eb91ac64e8feee34b7970c63be3feb159
Aluno 3 <aluno3@gmail.com>
2025-05-17 15:22:11 -0300

    criamos a pagina de menu

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#criando a funcao de criar um menu
+def menu():
+  while(True):
+    print("seja bem vindo(a) ao sistema de cadastro");
+    print("1.cadastrar um usuario")
+    print("2.listar missoes")
+    print("3.simular lancamentos")
+    print("4.sair");
+
+    opcao = input("escolha uma dessas opcoes");
+
+menu()
